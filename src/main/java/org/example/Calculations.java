1ea6b1d8d5a9f5de656a6fba64817233942a7a83
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-03-06 13:41:49 +0100

    Implementing IArithmeticsDiff interface method in Calculations class

--- src/main/java/org/example/Calculations.java
+++ src/main/java/org/example/Calculations.java
@@ -1,12 +1,15 @@
 package org.example;
 
 import org.example.Interfaces.IArithmeticsAdd;
+import org.example.Interfaces.IArithmeticsDiff;
 
-public class Calculations implements IArithmeticsAdd {
+public class Calculations implements IArithmeticsAdd, IArithmeticsDiff {
 
     @Override
     public double addition(double a, double b) {
         return a + b;
     }
 
+    @Override
+    public double difference(double A, double B) { return A - B; }
 }
