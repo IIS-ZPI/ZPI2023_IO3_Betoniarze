1b823c135505de9f774590320cc04a4ae210b7ee
mwteusz <240759@edu.p.lodz.pl>
2024-03-13 18:06:28 +0100

    add division functionality

--- src/main/java/org/example/Calculations.java
+++ src/main/java/org/example/Calculations.java
@@ -1,12 +1,20 @@
 package org.example;
 
 import org.example.Interfaces.IArithmeticsAdd;
+import org.example.Interfaces.IArithmeticsDiv;
 
-public class Calculations implements IArithmeticsAdd {
+public class Calculations implements IArithmeticsAdd, IArithmeticsDiv {
 
     @Override
     public double addition(double a, double b) {
         return a + b;
     }
 
+    @Override
+    public double division(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return a / b;
+    }
 }
