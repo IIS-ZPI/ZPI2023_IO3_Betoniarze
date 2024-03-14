3811ef5858d7ebb65002c9aafc4b0b092ff6fcd9
zurson <matu.jurek@gmail.com>
2024-03-14 11:03:37 +0100

    Some changes that improves code optimization and clarity

--- src/main/java/org/example/Calculations.java
+++ src/main/java/org/example/Calculations.java
@@ -1,9 +1,6 @@
 package org.example;
-import org.example.Interfaces.IArithmeticsAdd;
-import org.example.Interfaces.IArithmeticsDiff;
-import org.example.Interfaces.IArithmeticsDiv;
-import org.example.Interfaces.IArithmeticsMultiply;
-import org.example.Interfaces.IArithmeticsPower;
+
+import org.example.Interfaces.*;
 
 public class Calculations implements IArithmeticsAdd, IArithmeticsDiff, IArithmeticsDiv, IArithmeticsPower, IArithmeticsMultiply {
 
@@ -11,15 +8,17 @@ public class Calculations implements IArithmeticsAdd, IArithmeticsDiff, IArithme
     public double addition(double a, double b) {
         return a + b;
     }
-  
+
     @Override
-    public double difference(double A, double B) { return A - B; }
-  
+    public double difference(double A, double B) {
+        return A - B;
+    }
+
     @Override
     public double division(double a, double b) {
-        if (b == 0) {
+        if (b == 0)
             throw new IllegalArgumentException("Cannot divide by zero");
-        }
+
         return a / b;
     }
 
@@ -27,17 +26,10 @@ public class Calculations implements IArithmeticsAdd, IArithmeticsDiff, IArithme
     public double multiplication(double a, double b) {
         return a * b;
     }
-  
+
     @Override
     public double power(double a, double b) {
-        if(b == 0) return 1.0;
-        double exp = b;
-        double result = a;
-        if(b < 0) exp *= -1;
-        for(int i = 0 ; i < exp - 1; i++) {
-            result *= a;
-        }
-        if(b < 0) result = 1 / result;
-        return result;
+        return Math.pow(a, b);
     }
+
 }
