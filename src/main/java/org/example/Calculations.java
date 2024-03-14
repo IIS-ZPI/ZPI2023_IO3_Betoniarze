a2d665a484b14574e4bea7e358947a0be3ab783a
exaggeratedrumors <exaggeratedrumors@gmail.com>
2024-03-14 10:30:07 +0100

    IArithmeticsPower implementation and services

--- src/main/java/org/example/Calculations.java
+++ src/main/java/org/example/Calculations.java
@@ -3,8 +3,9 @@ package org.example;
 import org.example.Interfaces.IArithmeticsAdd;
 import org.example.Interfaces.IArithmeticsDiff;
 import org.example.Interfaces.IArithmeticsDiv;
+import org.example.Interfaces.IArithmeticsPower;
 
-public class Calculations implements IArithmeticsAdd, IArithmeticsDiff, IArithmeticsDiv {
+public class Calculations implements IArithmeticsAdd, IArithmeticsDiff, IArithmeticsDiv, IArithmeticsPower {
 
     @Override
     public double addition(double a, double b) {
@@ -20,5 +21,18 @@ public class Calculations implements IArithmeticsAdd, IArithmeticsDiff, IArithme
             throw new IllegalArgumentException("Cannot divide by zero");
         }
         return a / b;
+    }
+
+    @Override
+    public double power(double a, double b) {
+        if(b == 0) return 1;
+        double exp = b;
+        double result = a;
+        if(b < 0) exp *= -1;
+        for(int i = 0 ; i < exp - 1; i++) {
+            result *= a;
+        }
+        if(b < 0) result = 1 / result;
+        return result;
     }
 }
