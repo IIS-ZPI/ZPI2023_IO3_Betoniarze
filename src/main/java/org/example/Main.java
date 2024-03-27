98d8f2f792b5433fe8b53972c0cde0741f2ea0d7
thejerremi <thejerremi@gmail.com>
2024-03-27 20:03:05 +0100

    Revert "Changed comment about scrum master"
    This reverts commit 55875526f58f27cb8f497fb53f7d1f9f484ccd65.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Betoniarze: thejerremi DevOps"); // Scrum master and DevOps
+        System.out.println("Betoniarze: thejerremi DevOps"); // Scrum master
 
         printContributors();
     }
