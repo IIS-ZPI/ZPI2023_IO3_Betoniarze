98935ce2cc5c22a14985ae914280e78747224633
exaggeratedrumors <exaggeratedrumors@gmail.com>
2024-03-27 20:52:38 +0100

    Add comment

--- src/main/java/org/example/Interfaces/IArithmeticsPower.java
+++ src/main/java/org/example/Interfaces/IArithmeticsPower.java
@@ -2,5 +2,11 @@ package org.example.Interfaces;
 //comment3
 public interface IArithmeticsPower {
 
+    /**
+     * Abstract method calculating power of the number a with exponent b.
+     * @param a base number
+     * @param b exponent number
+     * @return result
+     */
     double power(double a, double b);
 }
